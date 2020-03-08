af0855f2a1d1edf934f8f57081abe383fe857f9b
Erick <erick.e.carrasco88@gmail.com>
2020-03-08 00:03:50 -0600

    Trabajando en el front-end, mejorar inicio y crear pagina de productos

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
 import React from 'react';
 //import logo from './logo.svg';
 //import './App.css';
-import {BrowserRouter as Router , Route , Switch} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { Home } from './Home';
 import { About } from './About';
 import { Contact } from './Contact';
 import { NoMatch } from './NoMatch';
+import { Layout } from './components/Layout';
+import { NavigationBar } from './components/NavigationBar';
+import { Jumbotron } from './components/Jumbotron';
 
 function App() {
   return (
     <React.Fragment>
-      <Router>
-        <Switch>
-          <Route exact path ="/" component = {Home} />
-          <Route path ="/about" component= {About} />
-          <Route path ="/contact" component = {Contact} />
-          <Route component={NoMatch} />
-        </Switch>
-      </Router>
+      <NavigationBar />
+      <Jumbotron />
+      <Layout>
+        <Router>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/about" component={About} />
+            <Route path="/contact" component={Contact} />
+            <Route component={NoMatch} />
+          </Switch>
+        </Router>
+      </Layout>
     </React.Fragment>
   );
 }
